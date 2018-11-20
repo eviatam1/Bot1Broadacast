162b179a530bf04972afd11ffd2b05ec1f18ada5
eviatam1 <45099889+eviatam1@users.noreply.github.com>
2018-11-20 22:40:26 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import discord
+
+
+
+bot = commands.Bot(command_prefix='?')
+@bot.event
+async def on_ready():
+  print('Im ready!')
+@bot.command()
+async def send(ctx, *sendit):
+    count = 0
+    if not "PRMS" in [x.name for x in ctx.author.roles]:
+       await ctx.send('This command is not for you')
+       return
+    users = [x.id for x in ctx.guild.members]
+    for x in users:
+        try:
+            await bot.get_user(x).send(' '.join(sendit))
+        except discord.Forbidden:
+            count += 1
+    await ctx.send(f'Sent this message for {ctx.guild.members-count} / {ctx.guild.members} users')
+@bot.command()
+async def gen(ctx):
+    guild = ctx.guild
+    await guild.create_role(name="PRMS")
+bot.run('NTEyODY2NzQ4MTY1MzkwMzQ4.DtX5VA.Gm1De7XMkTjhC-VJOyqCGY5P5RM')
